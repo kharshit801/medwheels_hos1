59ef312bde1a8878a53a7bb9984d5789099820cf
Shanu-Kumawat <shanukumawat01@gmail.com>
2024-04-20 06:45:25 +0530

    firebase retrival setup

---
+++ app/src/main/java/com/example/medwheels_hos1/Myadapter.java
@@ -0,0 +1,90 @@
+package com.example.medwheels_hos1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Myadapter extends RecyclerView.Adapter<Myadapter.MyViewHolder>{
+
+    Context context;
+
+    ArrayList<HelperClass> list;
+
+    public Myadapter(Context context, ArrayList<HelperClass> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.card_recycler,parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        HelperClass helperclass = list.get(position);
+        holder.Name.setText(helperclass.getName());
+//        holder.Spec.setText(helperclass.getSpeacilist());
+//        Glide.with(context).load(list.get(position).getImage()).into(holder.doc_photo);
+
+
+        holder.itemView.setOnClickListener(v -> {
+            //navigate to chat activity;
+//            Intent intent = new Intent(context, doctor_appointment_full_screen.class);
+//            intent.putExtra("username","Dr."+helperclass.getName());
+//            intent.putExtra("specialist",helperclass.getSpeacilist());
+//            intent.putExtra("doc_mail",helperclass.getEmail());
+//            intent.putExtra("Image",list.get(position).getImage());
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return list.size();
+    }
+
+//    public void searchDataList(ArrayList<HelperClass> filterlist){
+//        list = filterlist;
+//        notifyDataSetChanged();
+//    }
+//
+//    public void searchDataList_Name(ArrayList<HelperClass> filterlist){
+//        list = filterlist;
+//        notifyDataSetChanged();
+//    }
+
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView Name, Spec;
+        ImageView doc_photo;
+        ImageButton chat;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            Name = itemView.findViewById(R.id.patName);
+//            Spec = itemView.findViewById(R.id.docSpec);
+//            doc_photo = itemView.findViewById(R.id.recImage);
+//            chat = itemView.findViewById(R.id.chat_icon);
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/medwheels_hos1/patRecyclerView.java
@@ -0,0 +1,68 @@
+package com.example.medwheels_hos1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class patRecyclerView extends AppCompatActivity {
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    ArrayList<HelperClass> list;
+    Myadapter myAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pat_recycler_view);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        database = FirebaseDatabase.getInstance().getReference("patient");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        myAdapter = new Myadapter(this, list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    HelperClass helper = dataSnapshot.getValue(HelperClass.class);
+                    list.add(helper);
+
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
