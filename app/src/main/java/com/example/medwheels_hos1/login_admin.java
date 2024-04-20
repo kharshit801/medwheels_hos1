ab4e3eeae219aa7ba0313c92d2169853499c04d0
therev2 <revanchanna@gmail.com>
2024-04-20 08:23:24 +0530

    connected all the login activities

--- app/src/main/java/com/example/medwheels_hos1/login_admin.java
+++ app/src/main/java/com/example/medwheels_hos1/login_admin.java
@@ -1,6 +1,9 @@
 package com.example.medwheels_hos1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class login_admin extends AppCompatActivity {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_admin);
-
+        login=findViewById(R.id.klop);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(login_admin.this, patRecyclerView.class);
+                startActivity((intent));
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/medwheels_hos1/login_page_driver.java
+++ app/src/main/java/com/example/medwheels_hos1/login_page_driver.java
@@ -1,6 +1,12 @@
 package com.example.medwheels_hos1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +15,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class login_page_driver extends AppCompatActivity {
+    EditText email,pass;
+    TextView admin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page_driver);
+        email=findViewById(R.id.loginemail);
+        pass=findViewById(R.id.loginpass);
+        admin=findViewById(R.id.admin);
+
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(login_page_driver.this, login_admin.class);
+                startActivity((intent));
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
