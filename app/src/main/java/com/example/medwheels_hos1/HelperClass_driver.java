72912de9c60ca2459ce5697899bff6cb9d89c8cb
therev2 <revanchanna@gmail.com>
2024-04-20 19:03:52 +0530

    updated the helper class for driver location

--- app/src/main/java/com/example/medwheels_hos1/HelperClass_driver.java
+++ app/src/main/java/com/example/medwheels_hos1/HelperClass_driver.java
@@ -2,10 +2,13 @@ package com.example.medwheels_hos1;
 
 public class HelperClass_driver {
     String email,pass;
+    double latitude,longitude;
 
-    public HelperClass_driver(String email,String pass) {
+    public HelperClass_driver(String email,String pass,double latitude,double longitude) {
         this.email = email;
         this.pass=pass;
+        this.latitude=latitude;
+        this.longitude=longitude;
     }
 
     public String getEmail() {
@@ -22,5 +25,21 @@ public class HelperClass_driver {
 
     public void setPass(String pass) {
         this.pass = pass;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
 }
