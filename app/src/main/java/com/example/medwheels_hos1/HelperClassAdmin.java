98a93cbc1ed4e34c2246d8c1bce87c1e55ba38e9
therev2 <revanchanna@gmail.com>
2024-04-20 10:03:01 +0530

    connected admin register to firebase

---
+++ app/src/main/java/com/example/medwheels_hos1/HelperClassAdmin.java
@@ -0,0 +1,26 @@
+package com.example.medwheels_hos1;
+
+public class HelperClassAdmin {
+    String email,pass;
+
+    public HelperClassAdmin(String email, String pass) {
+        this.email = email;
+        this.pass = pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
--- app/src/main/java/com/example/medwheels_hos1/login_admin.java
+++ app/src/main/java/com/example/medwheels_hos1/login_admin.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,18 +12,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class login_admin extends AppCompatActivity {
     Button login;
-
+    EditText email,pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_admin);
         login=findViewById(R.id.klop);
+        email=findViewById(R.id.loginemail);
+        pass=findViewById(R.id.loginpass);
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String mail=email.getText().toString().trim(),pwd=pass.getText().toString().trim();
+                FirebaseDatabase database = FirebaseDatabase.getInstance("https://medwheels-4b07d-default-rtdb.asia-southeast1.firebasedatabase.app");
+                DatabaseReference reference = database.getReference("Admin");
+
+                HelperClassAdmin helperClass = new HelperClassAdmin(mail, pwd);
+                reference.child(mail.replace(".", ",")).setValue(helperClass);
                 Intent intent=new Intent(login_admin.this, patRecyclerView.class);
                 startActivity((intent));
             }
